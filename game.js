13e8e38ee4fc48d17ba5f6e015d25ebda5997410
Claire Tang <myfavecolorisgreen@gmail.com>
2014-06-16 19:06:16 -0700

    added new backgrounds and walking/firing animations to characters

--- game.js
+++ game.js
@@ -6,8 +6,8 @@ var myState = new Kiwi.State('myState');
 myState.preload = function(){
 	Kiwi.State.prototype.preload.call(this);
 	//this.addTextureAtlas('textureAtlas','spritesheet.png','textureAtlasJSON','spritesheet.json');
-	this.addSpriteSheet('characters','spritesheet.png',59,77);
-	this.addImage('background','f22_A.png');
+	this.addSpriteSheet('characters','character_spritesheet.png',54,64);
+	this.addImage('background','canvas_2.png');
 	this.addImage('row_of_blocks','Block_row.png');
 }
 
@@ -26,9 +26,14 @@ myState.create = function(){
 	this.blue_rightKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.D);
 	this.blue_upKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.W);
 	this.blue_downKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.S);
+	this.blue_fireKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.SHIFT);
 
-	this.blue.animation.add('idleleft',[2],0.1,false);
-	this.blue.animation.add('idleright',[3],0.1,false);
+	this.blue.animation.add('idleleft',[31],0.1,false);
+	this.blue.animation.add('idleright',[16],0.1,false);
+	this.blue.animation.add('moveright',[17,18,19,20,21,22],0.1,true);
+	this.blue.animation.add('moveleft',[30,29,28,27,26,25],0.1,true);
+	this.blue.animation.add('fireleft',[24],0.1,false);
+	this.blue.animation.add('fireright',[23],0.1,false);
 
 	this.blue_facing = 'left';
 	this.blue.animation.play('idleleft');
@@ -39,14 +44,19 @@ myState.create = function(){
 	this.red_rightKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.RIGHT);
 	this.red_upKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.UP);
 	this.red_downKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.DOWN);
+	this.red_fireKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.SPACEBAR);
 
-	this.red.animation.add('idleleft',[0],0.1,false);
-	this.red.animation.add('idleright',[1],0.1,false);
+	this.red.animation.add('idleleft',[15],0.1,false);
+	this.red.animation.add('idleright',[0],0.1,false);
+	this.red.animation.add('moveright',[1,2,3,4,5,6],0.1,true);
+	this.red.animation.add('moveleft',[14,13,12,11,10,9],0.1,true);
+	this.red.animation.add('fireright',[7],0.1,false);
+	this.red.animation.add('fireleft',[8],0.1,false);
 
 	this.red_facing = 'left';
 	this.red.animation.play('idleleft');
 
-	this.background = new Kiwi.GameObjects.StaticImage(this, this.textures['background'],20,20);
+	this.background = new Kiwi.GameObjects.StaticImage(this, this.textures['background'],0,0);
 	this.ground = new Kiwi.GameObjects.StaticImage(this, this.textures['row_of_blocks'],400,373);
 
 	this.addChild(this.background);
@@ -66,19 +76,26 @@ myState.update = function(){
 		this.blue_facing = 'right';
 		if(this.blue.transform.x<1500)
 			this.blue.transform.x+=3;
-		if(this.blue.animation.currentAnimation.name != 'idleright')
-			this.blue.animation.play('idleright');
+		if(this.blue.animation.currentAnimation.name != 'moveright')
+			this.blue.animation.play('moveright');
 	}
 	else if(this.blue_leftKey.isDown){
 		this.blue_facing = 'left';
 		if(this.blue.transform.x>3)
 			this.blue.transform.x-=3;
-		if(this.blue.animation.currentAnimation.name != 'idleleft')
-			this.blue.animation.play('idleleft');
+		if(this.blue.animation.currentAnimation.name != 'moveleft')
+			this.blue.animation.play('moveleft');
 	}
 	else if(this.blue_downKey.isDown){
 		if(this.blue.transform.y<600)
 			this.blue.transform.y+=3;
+	}
+	else if(this.blue_fireKey.isDown){
+		this.blue.animation.play('fire' + this.blue_facing);
+	}
+	else {
+		if(this.blue.animation.currentAnimation.name != 'idle' + this.blue_facing)
+			this.blue.animation.play('idle' + this.blue_facing);
 	}
 
 	if(this.red_upKey.isDown){
@@ -89,19 +106,26 @@ myState.update = function(){
 		this.red_facing = 'right';
 		if(this.red.transform.x<1500)
 			this.red.transform.x+=3;
-		if(this.red.animation.currentAnimation.name != 'idleright')
-			this.red.animation.play('idleright');
+		if(this.red.animation.currentAnimation.name != 'moveright')
+			this.red.animation.play('moveright');
 	}
 	else if(this.red_leftKey.isDown){
 		this.red_facing = 'left';
 		if(this.red.transform.x>3)
 			this.red.transform.x-=3;
-		if(this.red.animation.currentAnimation.name != 'idleleft')
-			this.red.animation.play('idleleft');
+		if(this.red.animation.currentAnimation.name != 'moveleft')
+			this.red.animation.play('moveleft');
 	}
 	else if(this.red_downKey.isDown){
 		if(this.red.transform.y<600)
 			this.red.transform.y+=3;
+	}
+	else if(this.red_fireKey.isDown){
+		this.red.animation.play('fire' + this.red_facing);
+	}
+	else {
+		if(this.red.animation.currentAnimation.name != 'idle' + this.red_facing)
+			this.red.animation.play('idle' + this.red_facing);
 	}
 }
 
