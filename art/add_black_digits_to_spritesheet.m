d803dd3a369f66ad8aa686c1d70358e386b753e8
Claire Tang <myfavecolorisgreen@gmail.com>
2014-12-03 21:21:27 -0800

    updating spritesheet to have black in between screen digits

---
+++ art/add_black_digits_to_spritesheet.m
@@ -0,0 +1,40 @@
+[img, map, alpha] = imread('black0_50.png');
+img_new(501:550,401:450,:) = img; 
+alpha_new(501:550, 401:450) = alpha;
+
+[img, map, alpha] = imread('black1_50.png');
+img_new(501:550,451:500,:) = img; 
+alpha_new(501:550, 451:500) = alpha;
+
+[img, map, alpha] = imread('black2_50.png');
+img_new(501:550,501:550,:) = img; 
+alpha_new(501:550, 501:550) = alpha;
+
+[img, map, alpha] = imread('black3_50.png');
+img_new(501:550,551:600,:) = img; 
+alpha_new(501:550, 551:600) = alpha;
+
+[img, map, alpha] = imread('black4_50.png');
+img_new(501:550,601:650,:) = img; 
+alpha_new(501:550, 601:650) = alpha;
+
+[img, map, alpha] = imread('black5_50.png');
+img_new(501:550,651:700,:) = img; 
+alpha_new(501:550, 651:700) = alpha;
+
+[img, map, alpha] = imread('black6_50.png');
+img_new(501:550,701:750,:) = img; 
+alpha_new(501:550, 701:750) = alpha;
+
+[img, map, alpha] = imread('black7_50.png');
+img_new(501:550,751:800,:) = img; 
+alpha_new(501:550, 751:800) = alpha;
+
+[img, map, alpha] = imread('black8_50.png');
+img_new(501:550,801:850,:) = img; 
+alpha_new(501:550, 801:850) = alpha;
+
+[img, map, alpha] = imread('black9_50.png');
+img_new(501:550,851:900,:) = img; 
+alpha_new(501:550, 851:900) = alpha;
+
