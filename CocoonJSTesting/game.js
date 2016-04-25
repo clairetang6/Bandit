e5332be7a1670a030ea225408d662921db113a69
Claire Tang <clairetang6@gmail.com>
2016-04-25 15:46:53 -0700

    remove Cocoon stuff
    Former-commit-id: f2ca85a7f16387c33a7fadc680ba8fabce933eca

--- CocoonJSTesting/game.js
+++
@@ -1,8 +0,0 @@
-var myGame = new Kiwi.Game(' ', 'myGame', null, {deviceTarget:Kiwi.TARGET_COCOON});
-//var myGame = new Kiwi.Game();
-console.log(myGame);
-myGame.states.addState(gameState);
-myGame.states.switchState('gameState');
-
-
-
